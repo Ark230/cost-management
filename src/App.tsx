f415d908ed96702b8092c0dc20c7da5d5027ae8a
Ignacio Pérez <iperez@jumpitt.com>
2023-09-21 10:28:14 -0300

    expenses table was added, bd products are now being fetched into it

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "@/application/config/redux/store";
 import ExpenseTracker from "./app/containers/ExpenseTracker/ExpenseTracker";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("America/Santiago");
 
 const App: React.FC = () => {
   return (
